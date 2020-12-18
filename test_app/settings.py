7ebb565bbdcfe85068ff647a72bc86db81f1693a
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2020-12-18 20:17:30 +0100

    django mysql app
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>

--- test_app/settings.py
+++ test_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'test_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysqldb',
+        'USER': 'test_app',
+        'PASSWORD': 'test_pass',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
